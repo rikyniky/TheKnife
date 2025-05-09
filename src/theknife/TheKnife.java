912209c5a940bed0fb8384ae6a437b6e5ba84508
Riccardo <rikyniky2000@gmail.com>
2025-05-09 18:46:05 +0200

    Commit INIZIALE

---
+++ src/theknife/TheKnife.java
@@ -0,0 +1,15 @@
+//Main.java
+package theknife;
+
+/**
+* Classe principale per l'avvio dell'applicazione TheKnife
+* @author [Nome Cognome]
+* @matricola [Numero Matricola]
+* @sede [VARESE]
+*/
+public class TheKnife {
+ public static void main(String[] args) {
+     TheKnife app = new TheKnife();
+     app.run();
+ }
+}
